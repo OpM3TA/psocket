216822d6913f4a983b8b33d9c296bb958e9a06b8
OpM3TA <xmodfree@gmail.com>
2015-03-30 18:48:49 -0400

    Update ex.main.cpp

--- ex.main.cpp
+++ ex.main.cpp
@@ -1,14 +1,17 @@
 #include "tcpsocket.h"
 
+
 using namespace std;
+
 
 int main()
 {
 	tcpsocket *sock = new tcpsocket();
 	sock->init();
-	int res = sock->pconnect("192.168.1.8", "1234");
-	if (res== 0) {
+	int testsock = sock->pconnect("192.168.1.8", "1234");
+	if (testsock == 0) {
 			sock->psend("Hello, World!\n");
 	}
+	delete sock;
 	getchar();
 }
