db6dbf1c139448b1de5722724ff50b4358b8db25
OpM3TA <xmodfree@gmail.com>
2015-03-30 18:56:58 -0400

    Update ex.main.cpp

--- ex.main.cpp
+++ ex.main.cpp
@@ -11,6 +11,9 @@ int main()
 	int testsock = sock->pconnect("192.168.1.8", "1234");
 	if (testsock == 0) {
 			sock->psend("Hello, World!\n");
+			char *msg = sock->precv(50); // Would recv 50 bytes of data that the server sends.
+			printf(msg);
+			delete[] msg; // pew pew
 	}
 	delete sock;
 	getchar();
