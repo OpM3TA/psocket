a265f1b5253664462f46776193262813c93b8f9f
OpM3TA <xmodfree@gmail.com>
2015-03-30 17:31:12 -0400

    Update ex.main.cpp

--- ex.main.cpp
+++ ex.main.cpp
@@ -6,22 +6,9 @@ using namespace std;
 
 int main()
 {
-	tcpsocket *s = new tcpsocket();
-	char * msg = s->pconnect("192.168.1.8", "5656").ErrMsg; // Try to connect & return status
-
-	if (msg== OK){
-		s->psend("Hello, friend\n");
-		char* data = s->precv(50);
-		printf("%s", data);
-	}
-	else if (msg == CreateSockErr)
-	{
-		printf("Could not create socket. -> socket()");
-	}
-	else if (msg == ConnectErr)
-	{
-		printf("Could not connect to the host -> connect()");
-	}
+	tcpsocket *sock = new tcpsocket();
+	sock->init();
+	int r = sock->pconnect("192.168.1.8", "1234");
+	printf("%d", r);
 	getchar();
-	
 }
